8d4d07fc0e03638664daec5563f039905fadc288
monmon8 <imaneben8@gmail.com>
2019-09-24 23:07:02 +0100

    adds print statement functionality

--- __test__/bank.spec.js
+++ __test__/bank.spec.js
@@ -25,4 +25,30 @@ describe("Test Feature", () => {
     expect(bank.balance).toEqual(200)
   });
 
+  it('adds transactions to the statement', () => {
+    mockdate.set('1/30/2000')
+    let d = new Date()
+    bank.addToStatement(100, 0, 100)
+    expect(bank.statement).toEqual([
+      {
+        date: d,
+        credit: 100,
+        debit: 0,
+        balance: 100
+      }
+    ])
+
+    mockdate.reset();
+  });
+
+  it('prints the statement', () => {
+    let d = new Date
+    const restoreConsole = mockConsole();
+    bank.deposit(300)
+    bank.withdrawal(100)
+    bank.printStatement()
+    expect(console.log).toHaveBeenCalledWith((`date || credit || debit || balance \n${d} || 300 || 0 || 300\n${d} || 0 || 100 || 200`));
+    restoreConsole();
+  });
+
 });
--- src/bank.js
+++ src/bank.js
@@ -10,11 +10,34 @@ Bank.prototype.currentDeposit = function () {
 
 Bank.prototype.deposit = function (amount) {
   this.balance += amount;
+  this.addToStatement(amount, 0, this.balance)
+
 };
 
 Bank.prototype.withdrawal = function (amount) {
   this.balance -= amount;
+  this.addToStatement(0, amount, this.balance)
 };
 
+
+Bank.prototype.addToStatement = function (credit, debit, balance) {
+  const x = {
+    date: new Date(),
+    credit: credit,
+    debit: debit,
+    balance: balance
+  }
+  this.statement.push(x)
+}
+
+Bank.prototype.printStatement = function () {
+  let a = "date || credit || debit || balance \n"
+
+  this.statement.forEach(function (x, i) {
+    a += `${x.date} || ${x.credit} || ${x.debit} || ${x.balance}\n`
+  })
+  console.log(a.substring(0, a.length - 1));
+
+};
 
 export default Bank;
